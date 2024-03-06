ac77e5532bc845b45ee6d236b53de9a489d96228
eR Em2 <erem2c@gmail.com>
2024-03-07 00:12:45 +0100

    Fixed react code

--- src/main.tsx
+++ src/main.tsx
@@ -4,36 +4,53 @@ import { Fragment } from 'react/jsx-runtime'
 
 const persons = [
   {
-    id: 1,
+    id: '1',
     photo: 'link',
     name: 'eR',
     lastName: 'Em',
     position: 'CTO',
     experience:[
-      {description: 'now', year: '2024' },
-      {description: 'now', year: '2024' },
+      {id: '1', description: 'now', year: '2024' },
+      {id: '2', description: 'now', year: '2024' },
     ],
     education:[
-      {institution: 'high', year: '2024', title: 'mgr'}
+      {id: '1', institution: 'high', year: '2024', title: 'mgr'}
     ]
   }
 ]
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<div>
-  {persons.map(me => (
-    <Fragment key={me.id}>
-    <header></header>
-    <div className='left'>
-      <div><img src="" alt="My photo" /></div>
-      <div>{me.name}</div>
-      <div>{me.lastName}</div>
-      <div>{me.position}</div>
-    </div>
-    <div>
-      <div className='right'></div>
-      <div></div>
-      <div></div>
-    </div>
-    <footer></footer>
-    </Fragment>))}
-</div>)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <div>
+    {persons.map(me => (
+      <Fragment key={me.id}>
+        <header></header>
+        <div className='left'>
+          <div><img src="" alt="My photo" /></div>
+          <div>{me.name}</div>
+          <div>{me.lastName}</div>
+          <div>{me.position}</div>
+        </div>
+        <div className='right'>
+          <div>
+            {me.experience.map(exp => (
+              <Fragment key={exp.id}>
+                <span>{exp.description}</span>
+                <span>{exp.year}</span>
+              </Fragment>
+            ))}
+          </div>
+          <div>
+            {me.education.map(edu => (
+              <Fragment key={edu.id}>
+                <span>{edu.institution}</span>
+                <span>{edu.year}</span>
+                <span>{edu.title}</span>
+              </Fragment>
+            ))}
+          </div>
+        </div>
+        <footer></footer>
+      </Fragment>
+    ))}
+  </div>
+)
