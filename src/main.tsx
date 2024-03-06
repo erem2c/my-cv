f2b534fbab6bab5d127d8b6b98248adf340084b5
eR Em2 <erem2c@gmail.com>
2024-03-06 23:29:26 +0100

    Created JSON data

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const me = [
+  {
+    id: 1,
+    photo: 'link',
+    name: 'eR',
+    lastName: 'Em',
+    position: 'CTO',
+    experience:[
+      {description: 'now', year: '2024' },
+      {description: 'now', year: '2024' },
+    ],
+    education:[
+      {institution: 'high', year: '2024', title: 'mgr'}
+    ]
+  }
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<div>
+
+  </div>)
